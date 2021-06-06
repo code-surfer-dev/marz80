a66bc84916ada3146a372053e2e8a39c11ad854f
Ingo Schmied <ingo@ingoneering.com>
2021-06-05 17:20:44 -0700

    save the latest changes for the 8255

--- marzbios.asm
+++ marzbios.asm
@@ -11,7 +11,7 @@
 
 STACK_TOP	.EQ $FFFF	Stack pointer starting address
 PORT_A		.EQ $00		8255 PORT A address - 16 x 2 LCD display
-PORT_B		.EQ $01		8255 PORT B address - Arduino Nano PS/2 kybd controller
+PORT_B		.EQ $01		8255 PORT B address - Not assigned
 PORT_C		.EQ $02		8255 PORT C address - control port for PORT A & PORT B
 PORT_CTL	.EQ $03		8255 Control register address
 
@@ -33,18 +33,19 @@ WARM_START
 	LD SP, STACK_TOP	Initialize the stack pointer
 
 ;---------------------------------------------------------------------------
-;		$86 Sets the 8255 GROUP A to MODE 0: PORT A & PORT C Upper to OUTPUTS
-;				GROUP B to MODE 1: PORT B INPUT, C Lower to STROBE
+;	$C2 Sets the 8255 GROUP A to MODE 2: PORT A bidirectional 
+;										 PORT C Upper to CTL
+;					  GROUP B to MODE 0: PORT B INPUT, C Lower to outputs
 ;---------------------------------------------------------------------------
 
-	LD A, $86
+	LD A, $C2
 	OUT (PORT_CTL), A	Send the control word to the 8255 Control Register
 
-	LD A, $38					Set LCD to 8 bit Mode, 2 lines, & 5x8 font
+	LD A, $38			Set LCD to 8 bit Mode, 2 lines, & 5x8 font
 	CALL LCD_COMMAND
-	LD A, $0F					Init display instruction
+	LD A, $0F			Init display instruction
 	CALL LCD_COMMAND
-	LD A, $01					Clear display instruction
+	LD A, $01			Clear display instruction
 	CALL LCD_COMMAND
 
 	LD HL, BIOS_MSG		Set HL to the bios message start address
@@ -53,33 +54,47 @@ NEXT_CHAR
 	CALL LCD_DISPLAY
 	INC HL
 	LD A, (HL)
-	CP $0A						Check for end of string
+	CP $0A				Check for end of string
 	JP NZ,NEXT_CHAR
 
-	LD A, $A8					Move cursor to the beginning of the 2nd line
+	LD A, $A8			Move cursor to the beginning of the 2nd line
 	CALL LCD_COMMAND
-	LD A, $3E					Load cursor '>'
+	LD A, $3E			Load cursor '>'
 	CALL LCD_DISPLAY
 	EI
 	HALT
 
 LCD_COMMAND
+	CALL LCD_READY
+	LD B, $04
+	OUT (PORT_C), B		Set E and RS 
 	OUT (PORT_A), A
-	LD A, $20
+	LD A, $05
 	OUT (PORT_C), A		Set E and reset RS to send data to command register
-	XOR A							Zero out accumulator
+	XOR A				Zero out accumulator
 	OUT (PORT_C), A
 	RET
 
 LCD_DISPLAY
+	CALL LCD_READY
 	OUT (PORT_A), A
 	LD A, $30
 	OUT (PORT_C), A		Set E and RS to send data to data register
-	XOR A							Zero out accumulator
+	XOR A				Zero out accumulator
 	OUT (PORT_C), A
+	RET
+
+LCD_READY
+	LD B, $06
+	OUT (PORT_C), B		Set the LCD R/W and E bits to read the busy flag
+	IN C, (PORT_A)		Read from the LCD
+	LD B, $02
+	OUT (PORT_C), B		Unset E of the LCD
+	RL C				Rotate bit 7 into the carry flag
+	JR C, LCD_READY		If the busy flag is set repeat the check
 	RET
 
 SCAN_CODE
 ;	IN A, (PORT_B)		Read the current scan code from keyboard controller
-; 									Conditional handling of scan codes for control or echo
+; 						Conditional handling of scan codes for control or echo
 	RET
